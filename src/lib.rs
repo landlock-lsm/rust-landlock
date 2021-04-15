d685e7b6c364e69a20ca48a7c7f775d7e6957d37
Mickaël Salaün <mic@digikod.net>
2021-04-15 17:28:08 +0200

    lib: Add AccessFs and Rule::new_path_beneath()
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,44 @@
+#[macro_use]
+extern crate bitflags;
+
 use libc::close;
 use std::io::Error;
 use std::mem::size_of_val;
-use std::os::unix::io::RawFd;
+use std::os::unix::io::{AsRawFd, RawFd};
 
 mod uapi;
+
+bitflags! {
+    pub struct AccessFs: u64 {
+        const EXECUTE = uapi::LANDLOCK_ACCESS_FS_EXECUTE as u64;
+        const WRITE_FILE = uapi::LANDLOCK_ACCESS_FS_WRITE_FILE as u64;
+        const READ_FILE = uapi::LANDLOCK_ACCESS_FS_READ_FILE as u64;
+        const READ_DIR = uapi::LANDLOCK_ACCESS_FS_READ_DIR as u64;
+        const REMOVE_DIR = uapi::LANDLOCK_ACCESS_FS_REMOVE_DIR as u64;
+        const REMOVE_FILE = uapi::LANDLOCK_ACCESS_FS_REMOVE_FILE as u64;
+        const MAKE_CHAR = uapi::LANDLOCK_ACCESS_FS_MAKE_CHAR as u64;
+        const MAKE_DIR = uapi::LANDLOCK_ACCESS_FS_MAKE_DIR as u64;
+        const MAKE_REG = uapi::LANDLOCK_ACCESS_FS_MAKE_REG as u64;
+        const MAKE_SOCK = uapi::LANDLOCK_ACCESS_FS_MAKE_SOCK as u64;
+        const MAKE_FIFO = uapi::LANDLOCK_ACCESS_FS_MAKE_FIFO as u64;
+        const MAKE_BLOCK = uapi::LANDLOCK_ACCESS_FS_MAKE_BLOCK as u64;
+        const MAKE_SYM = uapi::LANDLOCK_ACCESS_FS_MAKE_SYM as u64;
+    }
+}
 
 pub enum Rule {
     PathBeneath(uapi::landlock_path_beneath_attr),
 }
 
 impl Rule {
+    // We may want to duplicate the FD.
+    pub fn new_path_beneath<T>(fd: T, access: AccessFs) -> Rule where T: AsRawFd {
+        Rule::PathBeneath(uapi::landlock_path_beneath_attr {
+            allowed_access: access.bits,
+            parent_fd: fd.as_raw_fd(),
+        })
+    }
+
     fn as_ptr(&self) -> *const libc::c_void {
         match self {
             Rule::PathBeneath(attr) => attr as *const _ as _,
