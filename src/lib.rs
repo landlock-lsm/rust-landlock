43e59e4fbb54c1ec31a12f16065f5e8b21ed8380
Mickaël Salaün <mic@digikod.net>
2021-04-15 11:23:27 +0200

    lib: Start builder pattern
    Based on Vincent's patch.
    
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+use libc::close;
+use std::io::Error;
+use std::mem::size_of_val;
+use std::os::unix::io::RawFd;
+
 mod uapi;
 
-// TODO: Add high-level interface (e.g. ruleset builder) and tests.
+pub struct Ruleset {
+    fd: RawFd,
+}
+
+impl Ruleset {
+    pub fn new() -> Result<Ruleset, Error> {
+        // The API should be future-proof: one Rust program or library should have the same
+        // behavior if builded with an old or a newer crate (e.g. with an extended ruleset_attr
+        // enum).  It should then not be possible to give an "all-possible-handled-accesses" to the
+        // Ruleset builder because this value would be relative to the running kernel.
+        // FIXME: Will return -ENOMSG
+        let attr = uapi::landlock_ruleset_attr { handled_access_fs: 0 };
+
+        match unsafe { uapi::landlock_create_ruleset(&attr, size_of_val(&attr), 0) } {
+            fd if fd >= 0 => Ok(Ruleset { fd }),
+            _ => Err(Error::last_os_error()),
+        }
+    }
+}
+
+impl Drop for Ruleset {
+    fn drop(&mut self) {
+        unsafe {
+            close(self.fd);
+        }
+    }
+}
