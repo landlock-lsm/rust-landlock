eb9577460ed64b6a3e1e7857c71a0dec8feadee3
Mickaël Salaün <mic@digikod.net>
2021-04-15 11:21:22 +0200

    lib: Remove useless "extern"
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-extern crate libc;
-
 mod uapi;
 
 // TODO: Add high-level interface (e.g. ruleset builder) and tests.
--- src/uapi/mod.rs
+++ src/uapi/mod.rs
@@ -1,5 +1,3 @@
-extern crate libc;
-
 #[allow(dead_code)]
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
