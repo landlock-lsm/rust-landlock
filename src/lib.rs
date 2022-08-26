1e09cb01ba08ef14fd77f8b8759583e90c1a302b
Christian Duerr <chris.durr@phylum.io>
2022-08-26 15:32:58 +0200

    Take mutable reference in ruleset functions
    This patch changes the functions on the `RulesetCreated` struct to take
    `&mut self` instead of moving `self`.
    
    While taking `self` is extremely useful for `restrict_self` to ensure
    correct API usage, it can get very restrictive on the `add_rule(s)` /
    `set_no_new_privs` methods. When storing the `RulesetCreated` in your
    own struct for example, you'd have to make use of `Option` or move
    `self` yourself, making the design somewhat "viral".
    
    The following codeblock is made possible through this patch:
    
    ```rs
    struct MySandbox {
        ruleset: RulesetCreated,
    }
    
    impl MySandbox {
        fn new() -> Self {
            Self { ruleset: Ruleset::new().create().unwrap() }
        }
    
        fn add_rule(&mut self, rule: MyRuleFormat) {
            self.ruleset.add_rule(rule.into())?;
        }
    
        fn restrict_self(self) {
            self.ruleset.restrict_self().unwrap();
        }
    }
    ```
    
    I think this change improves the APIs flexibility while not
    significantly decreasing legibility for applications that do not require
    this flexibility.
    
    This patch also restructures the `add_rule` function a little bit to
    hopefully help with legibility.

--- src/lib.rs
+++ src/lib.rs
@@ -101,32 +101,35 @@ mod tests {
     use crate::*;
 
     fn ruleset_root_compat() -> Result<RestrictionStatus, TestRulesetError> {
-        Ok(Ruleset::new()
+        let mut ruleset = Ruleset::new()
             .handle_access(AccessFs::from_all(ABI::V1))?
-            .create()?
-            .add_rule(PathBeneath::new(
-                PathFd::new("/")?,
-                AccessFs::from_all(ABI::V1),
-            ))?
-            .restrict_self()?)
+            .create()?;
+
+        ruleset.add_rule(PathBeneath::new(
+            PathFd::new("/")?,
+            AccessFs::from_all(ABI::V1),
+        ))?;
+
+        Ok(ruleset.restrict_self()?)
     }
 
     fn ruleset_root_fragile() -> Result<RestrictionStatus, TestRulesetError> {
         // Sets default support requirement: abort the whole sandboxing for any Landlock error.
-        Ok(Ruleset::new()
+        let mut ruleset = Ruleset::new()
             // Must have at least the execute check…
             .set_best_effort(false)
             .handle_access(AccessFs::Execute)?
             // …and possibly others.
             .set_best_effort(true)
             .handle_access(AccessFs::from_all(ABI::V1))?
-            .create()?
-            .set_no_new_privs(true)
-            .add_rule(PathBeneath::new(
-                PathFd::new("/")?,
-                AccessFs::from_all(ABI::V1),
-            ))?
-            .restrict_self()?)
+            .create()?;
+        ruleset.set_no_new_privs(true);
+        ruleset.add_rule(PathBeneath::new(
+            PathFd::new("/")?,
+            AccessFs::from_all(ABI::V1),
+        ))?;
+
+        Ok(ruleset.restrict_self()?)
     }
 
     #[test]
